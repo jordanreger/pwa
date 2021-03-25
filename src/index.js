469b55fb50c4f55bcce439371488d4e481526f27
Jordan Reger <jordanreger@gmail.com>
2021-03-25 10:17:34 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import "./styles.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Lander />
+        </Route>
+        <Route path="/*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Lander() {
+  return (
+    <>
+      <span id="credits">
+        Built by the <a href="https://majel.me">majel.me</a> team.
+      </span>
+    </>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
 
 serviceWorker.register();
